c45665f2998030c0ad16403414309a11e9b9a43b
Simon Bechard <simon.bechard@gmail.com>
2024-09-28 22:06:39 +1000

    5 current   2024-09-28 21:51:42  24.05.5287.759537f06e69  6.6.52                          *

--- drives.nix
+++ drives.nix
@@ -6,14 +6,16 @@
   ...
 }: {
   # Data Drive Mount
-  fileSystems."/home/smissingham/a" = {
-    device = "/dev/nvme2n1p1";
-    fsType = "btrfs";
-    options = [
-      # If you don't have this options attribute, it'll default to "defaults"
-      # boot options for fstab. Search up fstab mount options you can use
-      "users" # Allows any user to mount and unmount
-      #"nofail" # Prevent system from failing if this drive doesn't mount
-    ];
+  fileSystems = {
+    "/run/media/simon/C " = {
+      device = "/dev/nvme0n1p3";
+      fsType = "ntfs3";
+      options = [
+        # If you don't have this options attribute, it'll default to "defaults"
+        # boot options for fstab. Search up fstab mount options you can use
+        "users" # Allows any user to mount and unmount
+        #"nofail" # Prevent system from failing if this drive doesn't mount
+      ];
+    };
   };
 }
