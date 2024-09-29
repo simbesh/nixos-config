49caf05f841e97a40e326cfc7e2780e2da913c48
Simon Bechard <simon.bechard@gmail.com>
2024-09-30 08:20:00 +1000

    17 current  2024-09-30 08:19:58  24.05.5287.759537f06e69  6.6.52                          *

--- all.nix
+++ all.nix
@@ -38,49 +38,51 @@ in {
   # Extra programs that can't/should'nt install via systemPackages
   services.flatpak.enable = true;
   programs.steam.enable = true; # so far, this is the best option. Flathub version less so, systemPackage version sucks
-  programs.firefox.enable = true;
   programs.dconf.enable = true; # https://github.com/NixOS/nixpkgs/issues/207339#issuecomment-1747101887
 
   programs.firefox = {
     # ff addon - https://addons.mozilla.org/en-US/firefox/addon/pwas-for-firefox/
-    # enable = true;
+    enable = true;
     # package = pkgs.firefox;
     nativeMessagingHosts.packages = [unstable.firefoxpwa];
   };
 
-  environment.systemPackages = with pkgs; [
-    (callPackage ./cursor.nix {})
-    unstable.firefoxpwa # ff addon - https://addons.mozilla.org/en-US/firefox/addon/pwas-for-firefox/
+  environment.systemPackages = with pkgs;
+    [
+      (callPackage ./cursor.nix {})
 
-    # System Utils
-    git
-    htop
-    pciutils
-    wget
-    alejandra # nix code formatter
+      # System Utils
+      git
+      htop
+      pciutils
+      wget
+      alejandra # nix code formatter
 
-    # Media Apps
-    spotify
+      # Media Apps
+      spotify
 
-    # Commumincation Apps
-    telegram-desktop
-    discord
+      # Commumincation Apps
+      telegram-desktop
+      discord
 
-    # Productivity Apps
-    libreoffice
-    obsidian
-    google-drive-ocamlfuse
-    thunderbird
-    teams-for-linux
+      # Productivity Apps
+      libreoffice
+      obsidian
+      google-drive-ocamlfuse
+      thunderbird
+      teams-for-linux
 
-    # Developer applications
-    git
-    vscode
-    # unstable.zed-editor # ironically, terribly slow. Probably just needs better nix support
-    # note, jetbrains products via systemPackages don't work. Use toolbox instead
-    jetbrains-toolbox
-    bruno
+      # Developer applications
+      git
+      vscode
+      # unstable.zed-editor # ironically, terribly slow. Probably just needs better nix support
+      # note, jetbrains products via systemPackages don't work. Use toolbox instead
+      jetbrains-toolbox
+      bruno
 
-    # SDKs
-  ];
+      # SDKs
+    ]
+    ++ (with unstable; [
+      firefoxpwa # ff addon - https://addons.mozilla.org/en-US/firefox/addon/pwas-for-firefox/
+    ]);
 }
